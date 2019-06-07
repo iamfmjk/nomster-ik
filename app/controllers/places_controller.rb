ba8f5efa8dc75e846a7f624762c5b0493359c9d3
Iuliia <julika27@gmail.com>
2019-06-07 03:12:41 +0000

    Added functionality for creation of a new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,21 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.order("name").page(params[:page]).per_page(4)
+    @places = Place.order(created_at: :desc).page(params[:page]).per_page(4)
   end
 
   def new
     @place = Place.new
   end
-  
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
