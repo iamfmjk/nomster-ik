19f649205b77e603eba3c9e578d8dbdd4d93a69b
Iuliia <julika27@gmail.com>
2019-06-13 06:03:31 +0000

    Added association for users and places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.order(created_at: :desc).page(params[:page]).per_page(4)
   end
@@ -8,7 +10,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ApplicationRecord
+
+  belongs_to :user
 end
