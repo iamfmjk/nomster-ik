0efa40e71ffd05e02e01aba235007e8f76a79637
Iuliia <julika27@gmail.com>
2019-06-14 23:50:31 +0000

    Fixed routes resources typo, created links for places and the place details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resource :places
+  resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
