1c8138c5ceae3be5f3bf22e145e588794ce6ccc8
Iuliia <julika27@gmail.com>
2019-06-21 19:05:12 +0000

    Added functionality for posting comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places
+  resources :places do
+    resources :comments, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
