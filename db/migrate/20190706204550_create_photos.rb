7836b1af1ba88aaa9414f4e8715323eaec30b07d
Iuliia <julika27@gmail.com>
2019-07-10 03:18:56 +0000

    Fixing migration for heroku deployment

--- db/migrate/20190706204550_create_photos.rb
+++ db/migrate/20190706204550_create_photos.rb
@@ -1,8 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.text :caption
-      t.integer :place_id
+      
       t.timestamps
     end
   end
