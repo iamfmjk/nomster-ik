d65c2f6370597bcdf42d58b430b8524d432df896
Iuliia <julika27@gmail.com>
2019-07-10 02:50:07 +0000

    Changed redirect for creating a new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,7 +12,7 @@ class PlacesController < ApplicationController
   def create
     @place = current_user.places.create(place_params)
     if @place.valid?
-      redirect_to root_path
+      redirect_to place_path(@place)
     else
       render :new, status: :unprocessable_entity
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :places
-         has_many :comments
-         has_many :photos
+  has_many :places
+  has_many :comments
+  has_many :photos
 end
