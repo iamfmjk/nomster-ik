dbd6aaa009985bcdda5eff3442daa87a5794b8c4
Iuliia <julika27@gmail.com>
2019-07-18 04:29:25 +0000

    Customized email contents

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "julika27@gmail.com", subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
