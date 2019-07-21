5065de883bb5286bc261269d38ca54ba0c3ecbb1
Iuliia <julika27@gmail.com>
2019-07-21 14:02:25 +0000

    Added controller, routes and resources for My Profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resources :photos
   end
+  resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
